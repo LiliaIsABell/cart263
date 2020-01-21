aa2657ce56c7c9580ce2ef9230f73d9acc2c17fa
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2020-01-21 16:57:11 -0500

    Add files via upload

---
+++ Assignments/Assignment2/Raving-Redactionist/js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/********************************************************************
+
+Raving Redactionist
+Lilia Isabel Aguirre Lugo
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+// Start the program
+$(document).ready(setup);
+
+// Variables
+const INTERVAL_LENGHT = 500;
+const PERCENTAGE = 0.1;
+let $spans;
+
+// Setup
+//
+function setup() {
+  // Variable
+  $spans = $('span');
+  // Set time interval that updates the spans
+  setInterval(update, INTERVAL_LENGHT);
+  // Set .on() function to make the
+  // clicking effect function
+  $spans.on('click', spanClicked);
+
+}
+// Update
+//
+function update() {
+  // All the spans will be updated
+  $spans.each(updateSpan);
+}
+
+// Update Span
+//
+function updateSpan() {
+  // Variable for random number
+  let num = Math.random();
+  // If the probability is less than 10%
+  // then the redacted text will be revealed
+  if (num < PERCENTAGE) {
+    $(this).removeClass("redacted");
+    $(this).addClass("revealed");
+  }
+}
+
+// Span Clicked
+//
+function spanClicked() {
+  // When the spans are clicked
+  // the text gets redacted
+  $(this).addClass("redacted");
+  $(this).removeClass("revealed");
+}
