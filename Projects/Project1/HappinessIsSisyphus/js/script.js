92b4a68d69dbc4cd41ba960b5eed208265317871
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2020-02-02 18:21:27 -0500

    P1: Added draggable and droppable
    There is a draggalble ghost image and a droppable flexbox. When the ghost is dropped on one of the squares, that square turns into a different color and the ghost dissapears.

--- Projects/Project1/HappinessIsSisyphus/js/script.js
+++ Projects/Project1/HappinessIsSisyphus/js/script.js
@@ -9,3 +9,34 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+// Variables
+let $ghost;
+let $square;
+
+
+// Start program
+$(document).ready(setup);
+
+// Setup
+//
+function setup(){
+$ghost = $('#ghost');
+$square = $('.square');
+
+// Ghost is draggable
+$ghost.draggable();
+// The squares are dropable
+$square.droppable({
+  drop: ghostCollected
+});
+
+}
+
+// Ghost Collected
+//
+function ghostCollected(event,ui){
+// When the ghost is dropped,
+// the color of the squares change
+$(this).addClass('collected');
+ui.draggable.remove();
+}
