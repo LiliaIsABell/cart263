d41546ce44c6e22d7ab00e3ba057b70cabc429f9
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2020-03-14 22:38:24 -0400

    A4: Added correct article
    When the first letter of the color is a vowel, the article will be "an" instead of "a".

--- Assignments/Assignment4/Condiments-Cacophony/js/script.js
+++ Assignments/Assignment4/Condiments-Cacophony/js/script.js
@@ -15,57 +15,67 @@ $(document).ready(setup);
 // Setup
 //
 function setup() {
-// JSON
-// Added crayola.json and genres.json to data.json
-$.getJSON("assets/data/data.json")
-.done(dataLoaded)
-.fail(dataError);
+  // JSON
+  // Added crayola.json and genres.json to data.json
+  $.getJSON("assets/data/data.json")
+    .done(dataLoaded)
+    .fail(dataError);
 }
 
 // Data Loaded
 //
-function dataLoaded(data){
+function dataLoaded(data) {
 
-// Random condiments are chosen
-let randomCondiment = getRandomElement(data.condiments);
+  // Random condiments are chosen
+  let randomCondiment = getRandomElement(data.condiments);
 
-// Uses correct verb
-let verb = "is";
-if (randomCondiment.charAt(randomCondiment.lenght - 1) === "s"){
-  verb = "are"
-}
+  // Random cat is chosen
+  let randomCat = getRandomElement(data.cats);
 
-// Random cat is chosen
-let randomCat = getRandomElement(data.cats);
+  // Random room is chosen
+  let randomRoom = getRandomElement(data.rooms);
 
-// Random room is chosen
-let randomRoom = getRandomElement(data.rooms);
+  // Random color chosen
+  // random element grabbed from colors
+  let colors = getRandomElement(data.colors);
+  // a color is chosen from colors
+  let randomColor = colors.color;
 
-// Random color chosen
-// random element grabbed from colors
-let colors = getRandomElement(data.colors);
-// a color is chosen from colors
-let randomColor = colors.color;
+  // Random music genre chosen
+  let randomGenre = getRandomElement(data.genres);
 
-// Random music genre chosen
-let randomGenre = getRandomElement(data.genres);
+  // Uses correct verb
+  let verb = "is";
+  if (randomCondiment.charAt(randomCondiment.lenght - 1) === "s") {
+    verb = "are";
+  }
 
-// Creates the description
-let condimentDescription = `${randomCondiment} ${verb} like a ${randomColor} ${randomCat} ${randomRoom} playing ${randomGenre} music`;
-// Adds it to the body
-$('body').append(condimentDescription);
+  // Uses correct indefinite articles
+  let article = "a"
+  let vowels = ["A", "E", "I", "O", "U"];
+  // create loop to search for vowels
+  for (let i = 0; i < vowels.length; i++) {
+    if (randomColor.charAt(randomColor[0]) === vowels[i]) {
+      article = "an";
+    }
+  }
+
+  // Creates the description
+  let condimentDescription = `${randomCondiment} ${verb} like ${article} ${randomColor} ${randomCat} ${randomRoom} playing ${randomGenre} music`;
+  // Adds it to the body
+  $('body').append(condimentDescription);
 }
 
 // Data error
 // Shows when there is an error with JSON
-function dataError(request, text, error){
-console.error(error);
+function dataError(request, text, error) {
+  console.error(error);
 }
 
 // Get random element
 //
-function getRandomElement(array){
-// chooses random element from the array chosen
-let element = array[Math.floor(Math.random()*array.length)];
-return element;
+function getRandomElement(array) {
+  // chooses random element from the array chosen
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
 }
